903f9aaa4f8b61e2c3c1f4ef0fa85616f2893f97
vienyevelyn <vieny.evelin@gmail.com>
2025-10-18 00:06:50 +0700

    fixing dab

--- koneksi.js
+++ koneksi.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("db_rumahsakit", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
 
 module.exports = sequelize;
